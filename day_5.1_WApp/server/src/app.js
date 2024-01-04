f2fbf63139cb06fb5183404ba6c9f62611a1bead
sobhiEgbaria <sobhim686@gmail.com>
2024-01-04 18:13:59 +0200

    styling

---
+++ day_5.1_WApp/server/src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { apiData } from "../../utils/weatherStackApi.js";
+import path from "path";
+import { dirname } from "path";
+
+const app = express();
+const port = 3000;
+
+const data = await apiData("Umm al-Fahm");
+
+app.get("/", (req, res) => {
+  res.send(`<h1>weather App<h1>
+  <h2>${data}<h1>
+  
+  `);
+});
+
+app.listen(port, () => {
+  console.log(`your server run in port ${port}`);
+});

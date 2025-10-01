5e3a9cead34489c2f51dd0417e66112d58e93a92
sobhiEgbaria <sobhim686@gmail.com>
2025-10-01 09:55:27 +0300

    clean up the  folders

--- dist/day_2_static_and_private/bankAccount.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-class BankAccount {
-    constructor(accountName, balance) {
-        this.accountName = accountName;
-        BankAccount.accountNumber = BankAccount.accountNumber + Date.now();
-        this.balance = balance;
-    }
-}
-BankAccount.accountNumber = 0;
--- dist/day_4_express_Social_Network_API/Comment.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-class Comment {
-    constructor(id, content, commentedBy) {
-        this.id = id;
-        this.content = content;
-        this.commentedBy = commentedBy;
-        // initialize other properties
-    }
-}

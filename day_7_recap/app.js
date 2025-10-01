59f1cc244a1ceb3c54360c63010b69acdf0fc6e5
sobhiEgbaria <sobhim686@gmail.com>
2025-10-01 12:40:55 +0300

    recap node req and res

--- day_7_recap/app.js
+++ day_7_recap/app.js
@@ -1,7 +1,21 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title><head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    res.write("<h1>your Message is accepted</h1>");
+    return res.end();
+  }
 });
 
 server.listen(3000);

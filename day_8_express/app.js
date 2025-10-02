a6bb06b4fdd9ed59792c3ac3b85a2a782ea2f075
sobhiEgbaria <sobhim686@gmail.com>
2025-10-02 15:28:38 +0300

    recap express

--- day_8_express/app.js
+++ day_8_express/app.js
@@ -1,1 +1,14 @@
-console.log("sleep");
+import express from "express";
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("the first mid");
+  next();
+});
+
+app.use((req, res, next) => {
+  res.send("hi the second mid");
+});
+
+app.listen(3000);
